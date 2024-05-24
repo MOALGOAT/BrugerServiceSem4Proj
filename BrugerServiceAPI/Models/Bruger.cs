81ccdac1896d5aea3a2d2820493985dc3d95df2f
jakobhaulund <jakobhaulund@hotmail.com>
2024-05-24 12:16:48 +0200

    nullable

--- BrugerServiceAPI/Models/Bruger.cs
+++ BrugerServiceAPI/Models/Bruger.cs
@@ -7,21 +7,21 @@ namespace BrugerServiceAPI.Models
     {
         [BsonId]
         public Guid _id { get; set; }
-        public string firstName { get; set; }
+        public string? firstName { get; set; }
 
-        public string lastName { get; set; }
-        public string email { get; set; }
+        public string? lastName { get; set; }
+        public string? email { get; set; }
 
-        public string adress { get; set; }
+        public string? adress { get; set; }
 
-        public string telephonenumber { get; set; }
+        public string? telephonenumber { get; set; }
 
-        public int role { get; set; }
+        public int? role { get; set; }
 
-        public string username { get; set; }
+        public string? username { get; set; }
 
-        public string password { get; set; } 
+        public string? password { get; set; } 
 
-        public DateTime created_at { get; set; } = DateTime.Now;
+        public DateTime? created_at { get; set; } = DateTime.Now;
     }
 }
