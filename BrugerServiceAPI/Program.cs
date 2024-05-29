71251df6bee7fc0b666c4b91833c3008a7e6df66
jakobhaulund <jakobhaulund@hotmail.com>
2024-05-29 15:54:25 +0200

    heeehardcode

--- BrugerServiceAPI/Program.cs
+++ BrugerServiceAPI/Program.cs
@@ -30,11 +30,11 @@ try
 
     var vaultService = new VaultService(configuration);
 
-    string mySecret = await vaultService.GetSecretAsync("secrets", "SecretKey");
-    string myIssuer = await vaultService.GetSecretAsync("secrets", "IssuerKey");
+    string mySecret = await vaultService.GetSecretAsync("secrets", "SecretKey") ?? "5Jw9yT4fb9T5XrwKUz23QzA5D9BuY3p6";
+    string myIssuer = await vaultService.GetSecretAsync("secrets", "IssuerKey") ?? "gAdDxQDQq7UYNxF3F8pLjVmGuU5u8g3y";
+    string myConnectionString = await vaultService.GetSecretAsync("secrets", "MongoConnectionString") ?? "mongodb://admin:1234@mongodb:27017";
 
-    string connectionString = await vaultService.GetConnectionStringAsync("secrets", "MongoConnectionString");
-    configuration["MongoConnectionString"] = connectionString;
+    configuration["MongoConnectionString"] = myConnectionString;
 
      builder.Services.AddAuthentication(options =>
     {
